05eb2c2587ae9c6aa13be6465e6a9647a23bc4a1
Michał Chęciński <michal.checinski@outlook.com>
2017-08-03 18:11:22 +0200

    Added project for logic

---
+++ src/TwitterComparerLibrary/TwitterOAuth.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TwitterApi.Tests
+{
+    public class TwitterOAuth
+    {
+        public static async Task<string> GetToken(string customerKey, string customerSecret)
+        {
+            var b64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(
+                $"{WebUtility.UrlEncode(customerKey)}:{WebUtility.UrlEncode(customerSecret)}"));
+            var req = new HttpRequestMessage(HttpMethod.Post, "https://api.twitter.com/oauth2/token");
+            req.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", b64);
+            req.Content = new FormUrlEncodedContent(new Dictionary<string, string>() {
+                { "grant_type", "client_credentials" }
+            });
+            var token = "";
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var res = await httpClient.SendAsync(req))
+                {
+                    if (res.IsSuccessStatusCode)
+                        token = Regex.Match(await res.Content.ReadAsStringAsync(), "\"access_token\":\"([^\"]+)")
+                            .Groups[1].Value;
+                }
+            }
+
+            return token;
+        }
+    }
+}
