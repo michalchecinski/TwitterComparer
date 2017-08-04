8f893237be2721eac3775c1087eb51569fcbd541
Michał Chęciński <michal.checinski@outlook.com>
2017-08-04 14:26:48 +0200

    Renamed class and added NextCursor prop

---
+++ src/TwitterComparerLibrary/JsonUsersRoot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TwitterComparerLibrary
+{
+    public class JsonUsersRoot
+    {
+        [JsonProperty("users")]
+        public List<User> Users { get; set; }
+
+        [JsonProperty("next_cursor")]
+        public long NextCursor { get; set; } = -1; 
+    }
+}
