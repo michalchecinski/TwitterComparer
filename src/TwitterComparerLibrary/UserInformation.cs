7a4cc17a627b54138141b30bb8d2c5af81f3ba26
Michał Chęciński <michal.checinski@outlook.com>
2017-08-04 12:12:32 +0200

    Added class UserInformation and tests for it... Class for getting detailed info about user

---
+++ src/TwitterComparerLibrary/UserInformation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TwitterComparerLibrary
+{
+    public class UserInformation
+    {
+        private readonly string _token;
+
+        public UserInformation(string token)
+        {
+            _token = token;
+        }
+
+        private async Task<string> GetResultAsync(string url)
+        {
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _token);
+
+            return await httpClient.GetAsync(url).Result.Content.ReadAsStringAsync();
+        }
+
+        public async Task<User> Get(string userName)
+        {
+            const string url = "https://api.twitter.com/1.1/users/show.json?screen_name=";
+
+            var json = await GetResultAsync(url + userName);
+
+            return JsonConvert.DeserializeObject<User>(json);
+        }
+    }
+}
---
+++ tests/TwitterComparerLibrary.Tests/get_info_about_user_tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TwitterComparerLibrary.Tests
+{
+    public class get_info_about_user_tests
+    {
+        private readonly string _customerKey;
+        private readonly string _customerSecret;
+        private readonly UserInformation _userInformation;
+
+        public get_info_about_user_tests()
+        {
+            _customerKey = ConfigurationManager.AppSettings["CustomerKey"];
+            _customerSecret = ConfigurationManager.AppSettings["CustomerSecret"];
+            _userInformation = new UserInformation(TwitterOAuth.GetToken(_customerKey, _customerSecret).Result);
+        }
+
+        [Fact]
+        public async Task get_info_about_user()
+        {
+            var user = await _userInformation.Get("mi_checinski");
+
+            User expectedUser = new User()
+            {
+                ScreenName = "mi_checinski",
+                Id = 1380219330
+            };
+
+            Assert.Equal(expectedUser, user);
+        }
+
+    }
+}
