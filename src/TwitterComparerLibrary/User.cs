c5f367c780bde25196327666899a090153d87b35
Michał Chęciński <michal.checinski@outlook.com>
2017-08-04 12:06:59 +0200

    Added Compare and HashCode methods to User class

--- src/TwitterComparerLibrary/User.cs
+++ src/TwitterComparerLibrary/User.cs
@@ -14,5 +14,31 @@ namespace TwitterComparerLibrary
         public string Url { get; set; }
         [JsonProperty("statuses_count")]
         public int StatusesCount { get; set; }
+        [JsonProperty("followers_count")]
+        public int FollowersCount { get; set; }
+        [JsonProperty("friends_count")]
+        public int FriendsCount { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            User u = obj as User;
+
+            if ((System.Object)u == null)
+            {
+                return false;
+            }
+
+            return u.Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
