76fbcfd59249726a386a4bf1a91cdc0ddc110389
Michał Chęciński <michal.checinski@outlook.com>
2017-08-06 17:06:21 +0200

    Changed class name form OAuthToken to OAuthTwitterToken

--- src/TwitterComparerLibrary/OAuthTwitterToken.cs
+++ src/TwitterComparerLibrary/OAuthTwitterToken.cs
@@ -8,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace TwitterComparerLibrary
 {
-    public class OAuthToken
+    public class OAuthTwitterToken
     {
-        public static async Task<string> Generate(string customerKey, string customerSecret)
+        public static async Task<string> GenerateAsync(string customerKey, string customerSecret)
         {
             var b64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(
                 $"{WebUtility.UrlEncode(customerKey)}:{WebUtility.UrlEncode(customerSecret)}"));
