8b3e4f059d5048dcef59be53426b3d2389f0d54a
Michał Chęciński <michal.checinski@outlook.com>
2017-08-06 17:06:37 +0200

    Rfactoring in TwitterApiRequestHandler class... Splited one method into several smaller to follow SRP

--- src/TwitterComparerLibrary/CompareUsersFriends.cs
+++ src/TwitterComparerLibrary/CompareUsersFriends.cs
@@ -35,9 +35,9 @@ namespace TwitterComparerLibrary
 
         public async Task<List<User>> GetCommonFriendsListAsync(string firstUserName, string secondUserName)
         {
-            if (lastFirstUser != firstUserName ||
-                lastSecondUser != secondUserName ||
-                LastUpdate > DateTime.Now.AddMinutes(-30))
+            //if (lastFirstUser != firstUserName ||
+            //    lastSecondUser != secondUserName ||
+            //    LastUpdate > DateTime.Now.AddMinutes(-30))
             {
                 const string url = "https://api.twitter.com/1.1/friends/list.json?screen_name=";
                 LastFriendsList = await TwitterApiRequestHandler.GetIntersectListAsync(firstUserName, secondUserName, url, _token);
--- src/TwitterComparerLibrary/TwitterApiRequestHandler.cs
+++ src/TwitterComparerLibrary/TwitterApiRequestHandler.cs
@@ -13,34 +13,34 @@ namespace TwitterComparerLibrary
 
         public static async Task<List<User>> GetIntersectListAsync(string firstUserName, string secondUserName, string url, string token)
         {
-
-            List<User> firstFollowersList = new List<User>();
-            JsonUsersRoot firstDeserialize = new JsonUsersRoot();
-
             _i = 0;
 
+            List<User> firstList = await GetAllUsersListAsync(firstUserName, url, token);
+
+            List<User> secondList = await GetAllUsersListAsync(secondUserName, url, token);
+
+            return GetIntersectUserList(firstList, secondList);
+        }
+
+        private static async Task<List<User>> GetAllUsersListAsync(string userName, string url, string token)
+        {
+            JsonUsersRoot firstDeserialize = new JsonUsersRoot();
+            List<User> firstList = new List<User>();
             while (firstDeserialize.NextCursor != 0)
             {
                 _i++;
-                var firstFollowers = await GetResultAsync(url + firstUserName + "&cursor="+ firstDeserialize.NextCursor, token);
-                firstDeserialize = JsonConvert.DeserializeObject<JsonUsersRoot>(firstFollowers);
-                var temp = firstFollowersList.Concat(firstDeserialize.Users);
-                firstFollowersList = temp.ToList();
+                var firstResult = await GetResultAsync(url + userName + "&cursor=" + firstDeserialize.NextCursor, token);
+                firstDeserialize = JsonConvert.DeserializeObject<JsonUsersRoot>(firstResult);
+                var temp = firstList.Concat(firstDeserialize.Users);
+                firstList = temp.ToList();
             }
 
-            List<User> secondFollowersList = new List<User>();
-            JsonUsersRoot secondDeserialize = new JsonUsersRoot();
+            return firstList;
+        }
 
-            while (secondDeserialize.NextCursor != 0)
-            {
-                _i++;
-                var secondFollowers = await GetResultAsync(url + secondUserName + "&cursor=" + secondDeserialize.NextCursor, token);
-                secondDeserialize = JsonConvert.DeserializeObject<JsonUsersRoot>(secondFollowers);
-                var temp = secondFollowersList.Concat(secondDeserialize.Users);
-                secondFollowersList = temp.ToList();
-            }
-
-            return firstFollowersList.Intersect(secondFollowersList).ToList();
+        private static List<User> GetIntersectUserList(IEnumerable<User> firstList, IEnumerable<User> secondList)
+        {
+            return firstList.Intersect(secondList).ToList();
         }
 
         public static async Task<string> GetResultAsync(string url, string token)
