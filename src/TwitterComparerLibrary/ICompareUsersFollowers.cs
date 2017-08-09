c7b281a0e79deff48f715067deca993182416fb0
Michał Chęciński <michal.checinski@outlook.com>
2017-08-09 14:56:02 +0200

    Added inerface for CompareUsersFollowers

---
+++ src/TwitterComparerLibrary/ICompareUsersFollowers.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TwitterComparerLibrary
+{
+    public interface ICompareUsersFollowers
+    {
+        Task<int> CommonFollowersNumberAsync(string firstUserName, string secondUserName);
+        Task<List<User>> GetCommonFollowersListAsync(string firstUserName, string secondUserName);
+    }
+}
