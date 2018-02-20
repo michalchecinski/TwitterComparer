b4a7664d67e5dc5c8e5a8db6452c43d90f449e80
Michał Chęciński <michal.checinski@outlook.com>
2018-02-20 16:46:58 +0100

    Added ASP.NET Core API project

---
+++ src/TwitterComparer.Api/Controllers/CompareController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TwitterComparerLibrary;
+
+namespace TwitterComparer.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Compare")]
+    public class CompareController : Controller
+    {
+        private readonly ICompare _compare;
+
+        public CompareController(ICompare compare)
+        {
+            _compare = compare;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get(string firstUsername, string secondUserName)
+        {
+            return _compare.CompareUsers( , firstUsername, secondUserName);
+        }
+    }
+}
---
+++ src/TwitterComparer.Api/Token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterComparerLibrary;
+
+namespace TwitterComparer.Api
+{
+    public class Token
+    {
+        private readonly ITokenProvider _tokenProvider;
+        public Token(ITokenProvider tokenProvider)
+        {
+            _tokenProvider = tokenProvider;
+        }
+    }
+}
