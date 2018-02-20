c0615fbb8c46d1449c5bce05fdf11499b272dbf2
Michał Chęciński <michal.checinski@outlook.com>
2018-02-20 16:32:27 +0100

    Added interface for friends comparation class

---
+++ src/TwitterComparerLibrary/ICompareUsersFriends.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TwitterComparerLibrary
+{
+    public interface ICompareUsersFriends
+    {
+        Task<int> GetCommonFriendsNumberAsync(string firstUserName, string secondUserName);
+        Task<List<User>> GetCommonFriendsListAsync(string firstUserName, string secondUserName);
+    }
+}
